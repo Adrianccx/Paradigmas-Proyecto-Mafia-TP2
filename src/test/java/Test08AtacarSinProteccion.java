934c8081bddf7cafee5672214de26243239092ab
Ignacio <icavieres@fi.uba.ar>
2026-06-11 03:51:16 +0000

    test08

---
+++ src/test/java/Test08AtacarSinProteccion.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Test07AtacarProtegido {
+    @Test
+    public void testAtacarProtegido() {
+        EstadoPartida estado = new EstadoPartida();
+        Jugador victima = new Jugador();
+        Jugador medico = new Jugador();
+        medico.setRol(new Medico());
+
+        estado.añadirJugador(victima);
+        estado.añadirJugador(medico);
+        medico.accionNocturna(victima);
+        estado.eliminarJugador(victima);
+
+        assertTrue(estado.estaVivo(victima), "La víctima debería estar viva.");
+    }
+}
