86ff9c2f77b29b232bba730befc5fa603060b385
Ignacio <icavieres@fi.uba.ar>
2026-06-11 02:12:00 +0000

    test07 inicializacion

--- src/test/java/Test07AtacarProtegido.java
+++ src/test/java/Test07AtacarProtegido.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
 public class Test07AtacarProtegido {
+    public void testAtacarProtegido() {
+        EstadoPartida estado = new EstadoPartida();
+        Jugador victima = new Jugador();
+        Jugador medico = new Jugador();
+        estado.añadirJugador(victima);
+        estado.añadirJugador(medico);
+
+    }
 }
