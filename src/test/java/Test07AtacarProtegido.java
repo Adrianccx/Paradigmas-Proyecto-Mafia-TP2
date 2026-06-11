cd53d5f63f156c145073895b49b0f0f3bf1a57ef
Ignacio <icavieres@fi.uba.ar>
2026-06-11 02:56:40 +0000

    test07

--- src/test/java/Test07AtacarProtegido.java
+++ src/test/java/Test07AtacarProtegido.java
@@ -1,10 +1,18 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class Test07AtacarProtegido {
     public void testAtacarProtegido() {
         EstadoPartida estado = new EstadoPartida();
         Jugador victima = new Jugador();
         Jugador medico = new Jugador();
+        medico.setRol(new Medico());
+
         estado.añadirJugador(victima);
         estado.añadirJugador(medico);
+        medico.accionNocturna(victima);
+        estado.eliminarJugador(victima);
 
+        assertTrue(estado.estaVivo(victima), "La víctima debería estar viva.");
     }
 }
