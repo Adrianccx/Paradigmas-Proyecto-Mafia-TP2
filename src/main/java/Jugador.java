d3f65e775df34ef70c0e198aaa9f0007b2ec5c75
Ignacio <icavieres@fi.uba.ar>
2026-06-11 02:02:53 +0000

    medico proteger

--- src/main/java/Jugador.java
+++ src/main/java/Jugador.java
@@ -1,9 +1,14 @@
 public class Jugador {
 
     private Rol rol;
+    private boolean protegido;
 
     public Rol getRol() {
         return this.rol;
+    }
+
+    public boolean estaProtegido() {
+        return this.protegido;
     }
 
     public void setRol(Rol rol) {
@@ -14,7 +19,10 @@ public class Jugador {
         if (this == solicitante) {
             return this.rol;
         }
-
         return null;
+    }
+
+    public void proteger() {
+        protegido = true;
     }
 }
--- src/main/java/Medico.java
+++ src/main/java/Medico.java
@@ -1,2 +1,5 @@
 public class Medico extends Rol {
+    public void protegerA(Jugador jugador) {
+        jugador.proteger();
+    }
 }
