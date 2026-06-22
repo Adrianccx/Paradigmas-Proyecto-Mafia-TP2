328a93dd749d0c0759d1593adef2155b35504c71
Ignacio <icavieres@fi.uba.ar>
2026-06-22 21:42:08 +0000

    test jugador

---
+++ src/main/java/interfaz/juego/BotonJugador.java
@@ -0,0 +1,10 @@
+package interfaz.juego;
+
+import javafx.scene.control.Button;
+import jugador.Jugador;
+
+public class BotonJugador extends Button {
+    public BotonJugador(Jugador jugador) {
+        setText("Jugador 0");
+    }
+}
--- src/main/java/interfaz/juego/EscenaJuego.java
+++ src/main/java/interfaz/juego/EscenaJuego.java
@@ -5,6 +5,7 @@ import interfaz.menu_principal.MenuPrincipal;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -13,16 +14,17 @@ public class EscenaJuego extends Scene {
     public EscenaJuego(Stage stage) {
         super(new Pane(), 800, 600);
 
-        Button botonJugar = new Button();
-        botonJugar.setText("Volver");
-        botonJugar.setOnAction(e -> {
+        Button botonVolver = new Button();
+        botonVolver.setText("Volver");
+        botonVolver.setOnAction(e -> {
             stage.setScene(new MenuPrincipal(stage));
         });
 
-        VBox contenedorPrincipal = new VBox(
-                botonJugar
-        );
-        contenedorPrincipal.setAlignment(Pos.CENTER);
-        setRoot(contenedorPrincipal);
+        Button botonJugador = new BotonJugador(null);
+        botonJugador.setAlignment(Pos.CENTER);
+        BorderPane panel = new BorderPane();
+        panel.setCenter(botonJugador);
+        panel.setTop(botonVolver);
+        setRoot(panel);
     }
 }
